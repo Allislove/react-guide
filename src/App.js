8ab30f3fab1908c70b5474116b163b83f2db021d
iamOther <iamother@evoluciondigital.com.co>
2021-10-05 21:21:04 -0500

    Added Clock Component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import './App.css';
+import Clock from './components/Clock';
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        <a
-            className="App-link"
-            href="https://github.com/Allislove/react-guide/branches"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Guide / Andrés Romaña
-          </a>
-      </h1>
 
-      <b> En el enlace de arriba, podras ir a cada una de las ramas para ver su contenido!</b>
+      <Clock />
 
-
-      <p> <a href="https://andresromana.com"> Y presionando aqui! podras ir a mi Blog </a> </p>
-
+      <p> <a href="https://andresromana.com"> Presionando aqui! podras ir a mi Blog </a> </p>
     </div>
   );
 }
